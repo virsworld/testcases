ff4cf3ad3b80dbea680a3c93fbd1f6b49c04c4ce
virsworld <virpatel71@gmail.com>
2025-01-06 01:15:30 -0500

    last fre

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home';
-import APS105 from './pages/home/aps105/aps105';
+import Home from './pages/Home/Home';
+import APS105 from './pages/Home/APS105/APS105';
 import ECE244 from './pages/Home/ECE244/ECE244';
-import About from './pages/about/about';
+import About from './pages/About/About';
 import Login from './pages/Login/Login';
 
 const routes = (
